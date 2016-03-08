ec39695ce7277feee63c35a6f2139e58086ddd0b
ardessau <ardessau@comcast.net>
2016-03-07 19:06:02 -0700

    Made more changes to app.js on 3/7/16

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
 console.log ('hello world');
 
+function sayHello(word) {
+    console.log("Hello " + word);
+}
+
+sayHello("Audrey");
+
 // Method 1
 var o = { };
 o.name = "Audrey";
 o.address = "123 Main Street"
 o.age = 19;
 o.age += 5;
+o.sayHello = function() {
+    console.log("Hello pbject method");
+}
+o.sayHello();
+
+o2.sayHello2 =  sayHello;
 
 // Method 2
 var o2 = {
     name: "Audrey",
     address: "123 Main Street",
-    age: 19 +5
+    age: 19 +5,
+
+    sayHello: function() {
+        console.log("Hello, object method!!");
+    },
+    fruit: ["Apples", "Oranges", "Kiwi"]
 };
+
+o2.sayHello();
 
 o2.age += 5;
 
+o2.name = "Ms. " + o2.name + " Dessauer";
 
-o2.name = "Ms. " + o2.name + " Dessauer"
+var myArray = ["Apples", "Oranges", "Kiwi"];
+
+console.log(myArray[0]);
+console.log(o2.fruit[0]);
+
 console.log(o);
+
 console.log(o2);
